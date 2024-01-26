5b002018278e0f187cd489648bb950479d35c474
kapanadze04 <kapanadze.gio96@gmail.com>
2024-01-26 17:30:36 +0400

    Add to html document

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 function fetchData(productId) {
     fetch(`https://dummyjson.com/products/${productId}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            document.getElementById("container").innerHTML += `
+                <section>
+                    <img src = "${data.thumbnail}" />
+                    <span>${data.title}</span>
+                </section>
+                
+            `
+        })
 }
 
 // Only 5 products
 for(let i = 1; i <= 5; i++) {
     fetchData(i)
 }
-
-
-
-
 
 
