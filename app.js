1491b126e03a5f24027915f1bc04b6d33489782a
kapanadze04 <kapanadze.gio96@gmail.com>
2024-01-26 23:44:25 +0400

    modal

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
+const modalBox = document.getElementById("modal-box")
+const modal = document.getElementById("modal")
+const mainPage = document.getElementById("main-Page")
+
+
 function fetchData(productId) {
     fetch(`https://dummyjson.com/products/${productId}`)
         .then(res => res.json())
         .then(data => {
-            document.getElementById("container").innerHTML += `
-                <section>
+            const container = document.getElementById("container")
+            const section = document.createElement('section')
+
+            section.innerHTML = `
+                <img src = "${data.thumbnail}" />
+                <span>${data.title}</span>
+            `;
+
+            section.id = `productSection${productId}`
+            container.appendChild(section)
+
+            section.addEventListener("click", function() {
+                // console.log(`section ${productId} clicked!`)
+                modalBox.style.display = "flex"
+                mainPage.style.backgroundColor = "#e5e5e5"
+
+                modal.innerHTML = `
                     <img src = "${data.thumbnail}" />
-                    <span>${data.title}</span>
-                </section>
+                    <ul>
+                        <li>${data.description}</li>
+                        <hr>
+                        <li>Brand: ${data.brand}</li>
+                        <li>Category: ${data.category}</li>
+                        <li>Price: ${data.price}</li>
+                        <li>Discount: ${data.discountPercentage}</li>
+                        <li>Rating: ${data.rating}</li>
+                        <li>Stock: ${data.stock}</li>
+                    </ul>
+                    <button>დახურვა</button>
+                    <button>წაშლა</button>
+                `
                 
-            `
+            })
         })
 }
 
