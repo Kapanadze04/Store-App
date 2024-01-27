fd79ef69f44fe1f12a2ee9796a7cdf4aa4da3c7a
kapanadze04 <kapanadze.gio96@gmail.com>
2024-01-27 20:00:27 +0400

    Button Functions

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const modalBox = document.getElementById("modal-box")
 const modal = document.getElementById("modal")
 const mainPage = document.getElementById("main-Page")
 
+const deletedBox = document.getElementById("deleted-box")
+const deletedProductsTitle = document.getElementById("deleted-products-title")
 
 function fetchData(productId) {
     fetch(`https://dummyjson.com/products/${productId}`)
@@ -19,7 +21,6 @@ function fetchData(productId) {
             container.appendChild(section)
 
             section.addEventListener("click", function() {
-                // console.log(`section ${productId} clicked!`)
                 modalBox.style.display = "flex"
                 mainPage.style.backgroundColor = "#e5e5e5"
 
@@ -35,11 +36,43 @@ function fetchData(productId) {
                         <li>Rating: ${data.rating}</li>
                         <li>Stock: ${data.stock}</li>
                     </ul>
-                    <button>დახურვა</button>
-                    <button>წაშლა</button>
-                `
+                    <button id="closeBtn">დახურვა</button>
+                    <button id="deleteBtn">წაშლა</button>
+                `;
+
+                const closeBtn = document.getElementById("closeBtn")
+                const deleteBtn = document.getElementById("deleteBtn")
+
+                closeBtn.addEventListener("click", function() {
+                    modalBox.style.display = "none";
+                    mainPage.style.backgroundColor = "";
+                })
+
+                deleteBtn.addEventListener("click", function() {
+                    const sectionToRemove = document.getElementById(`productSection${productId}`)
+
+                    container.removeChild(sectionToRemove);
+        
+                    mainPage.style.backgroundColor = ""
+                    modalBox.style.display = "none"
+                    deletedBox.style.display = "flex"
+                    deletedProductsTitle.textContent += `${data.title + ", "}`
+
+                    modal.innerHTML = ""
+                })
+
+                returnBtn.addEventListener("click", function() {
+                    
+                    section.id = `productSection${productId}`
+                    container.appendChild(section)
+
+                    deletedBox.style.display = ""
+                    deletedProductsTitle.textContent = ""
+                    
+                })
                 
             })
+
         })
 }
 
